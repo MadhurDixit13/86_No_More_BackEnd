59f7524a50c949a97c180ee955691b91ded24d9c
Devansh Shah <54364517+drs1951@users.noreply.github.com>
2023-10-09 23:57:24 -0400

    update model of menu

--- models/menu.js
+++ models/menu.js
@@ -1,28 +1,40 @@
 const mongoose = require("mongoose");
 
 const menuSchema = new mongoose.Schema({
-  restname: {
-    type: String,
-    required: true,
-  },
-  menuname: {
-    type: String,
-    required: true,
-  },
-  restid:{
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  quantity: {
-    type: Number,
-    default: 0,
-  },
-  costmenu: {
-    type: Number,
-    required: true,
-  },
-  
+	restname: {
+		type: String,
+		required: true,
+	},
+	menuname: {
+		type: String,
+		required: true,
+	},
+	restid: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	quantity: {
+		type: Number,
+		default: 0,
+	},
+	costmenu: {
+		type: Number,
+		required: true,
+	},
+	ingredients: [
+		{
+			inventory_id: {
+				type: mongoose.Types.ObjectId,
+				required: true,
+				ref: "Inventory",
+			},
+			quantity: {
+				type: Number,
+				required: true,
+			},
+		},
+	],
 });
 
 const Menu = mongoose.model("Menu", menuSchema);
